1f80330f6e908861df456b047ebaf88b87947a31
Isaamarod <35488726+Isaamarod@users.noreply.github.com>
2018-06-15 09:59:01 +0200

    Update copy_folders_and_names.sh

--- copy_folders_and_names.sh
+++ copy_folders_and_names.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+#If you're using OS windows install Cywing to execute .sh
 #origine rute
 rutaori=procesadas_sin_train/*
 
@@ -23,6 +24,8 @@ do
 #copy the archives in  destination route
 
 cp $archivos $rutadest
+#windows
+#xcopy $archivos $rutadest
 
 #for each copied archive in destination route 
 for archivos_copiados in "$rutadest"
@@ -31,6 +34,8 @@ do
 # enter in destination route and put the name of each archive
 cd  "${rutadest}"
 ls>../etiquetas_sin_train.txt
+#windows
+#dir>../etiquetas_sin_test.txt
 done
 done
 done
